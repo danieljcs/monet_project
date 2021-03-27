4b206863c48b9d9b64857d84062d6ebe893eeb64
danieljcs <50522425+danieljcs@users.noreply.github.com>
2021-03-27 01:55:24 -0500

    arreglos
    modificacion en campos de modelos para aceptar nulos
    daniel jose castillo salej

--- monet_project/settings.py
+++ monet_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'monet_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_monet.sqlite3',
     }
 }
 
